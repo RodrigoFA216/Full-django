315e950654fe498cd3dc60db57b74029936a67db
Rodrigo Flores <rfloresa1900@alumno.ipn.mx>
2023-02-19 20:58:22 -0600

    add views and problem in signing

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 #con HttpResponse podemos regresar un texto 
@@ -54,3 +54,27 @@ def signup(request):
     
 def tasks(request):
     return render(request,'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request,'signin.html',{
+            'form': AuthenticationForm
+        })
+    elif request.method == 'POST':
+        print(request.POST["username"], request.POST["password"])
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user=authenticate(request, username=username, password=password)
+        print(user)
+        if user is None:
+            return render(request,'signin.html',{
+                'form': AuthenticationForm,
+                'error': 'Ususario o contraseña incorrecta'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
